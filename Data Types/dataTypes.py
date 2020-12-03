25962f3baa11495b057ff8f9f2ec61210e0867b9
Alexander Nitiola <cre8tor.alexander@gmail.com>
2020-12-03 19:31:44 +0100

    Completed the Int and the Float Data Section

---
+++ Data Types/dataTypes.py
@@ -0,0 +1,36 @@
+
+# 1. FUNDAMENTAL DATA TYPES 
+
+# 1. Int
+# 2. Float
+# 3. bool   - boolean
+# 4. str    - string
+# 5. list
+# 6. tuple
+# 7. set
+# 8. dict   - dictionary
+
+
+# 2. CLASSES DATA TYPES -> Custom Types
+
+# 3. SPECIALIZED DATA TYPES
+
+# SPECIAL DATA TYPE
+None
+
+
+# EXPLANATION
+
+# Init and float
+# number = int('10')
+# print(type(number))
+
+# print((2+4))        # Int
+# print(int(4/2))     # Int
+
+# print(type(2-4))    # Int
+# print(type(2 / 4)) # 0.5 - Float
+
+print(2 ** 3)
+print(5 // 4)
+print((6 % 4))
